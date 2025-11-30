3131d1a816b9c954381cc0da00cca9744992459a
Batcave765 <msriram765@gmail.com>
2025-11-30 13:26:50 +0530

    remove redudndant code

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,23 +1,16 @@
-import { Button } from '@/components/ui/button';
-import { Icon } from '@/components/ui/icon';
-import { Text } from '@/components/ui/text';
 import { Card, sampleData } from '@/components/ui/card';
 import { Chart } from '@/components/ui/chart';
-import { Link, Stack } from 'expo-router';
-import { MoonStarIcon, StarIcon, SunIcon } from 'lucide-react-native';
-import { useColorScheme } from 'nativewind';
+import { Stack } from 'expo-router';
 import * as React from 'react';
 import { ScrollView, View } from 'react-native';
 
 export default function Screen() {
-  const { colorScheme } = useColorScheme();
 
   return (
     <>
       <Stack.Screen />
       <ScrollView>
         <View className="flex-1 items-center justify-center p-4 mt-[50px]">
-          {/* <Text>Home</Text> */}
           <View className="w-full gap-4">
             <Chart
               total={`$${sampleData
