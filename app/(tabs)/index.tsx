fa4b6b42956aeccbdb767feba2ed87aa6808edff
Batcave765 <msriram765@gmail.com>
2025-11-30 11:28:20 +0530

    Added a Total Spending card

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,6 +2,7 @@ import { Button } from '@/components/ui/button';
 import { Icon } from '@/components/ui/icon';
 import { Text } from '@/components/ui/text';
 import { Card, sampleData } from '@/components/ui/card';
+import { Chart } from '@/components/ui/chart';
 import { Link, Stack } from 'expo-router';
 import { MoonStarIcon, StarIcon, SunIcon } from 'lucide-react-native';
 import { useColorScheme } from 'nativewind';
@@ -18,6 +19,16 @@ export default function Screen() {
         <View className="flex-1 items-center justify-center p-4 mt-[50px]">
           {/* <Text>Home</Text> */}
           <View className="w-full gap-4">
+            <Chart
+              total={`$${sampleData
+                .reduce((acc, curr) => {
+                  const price = parseFloat(
+                    curr.price.replace('$', '').replace('/month', '')
+                  );
+                  return acc + price;
+                }, 0)
+                .toFixed(2)}`}
+            />
             {sampleData.map((card, index) => (
               <Card key={index} {...card} />
             ))}
