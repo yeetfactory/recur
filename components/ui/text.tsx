21ee9968a1cfa0b606e591978fbac4a0abcb9129
Batcave765 <msriram765@gmail.com>
2025-12-04 20:39:43 +0530

    Updated Fonts

--- components/ui/text.tsx
+++ components/ui/text.tsx
@@ -6,7 +6,7 @@ import { Platform, Text as RNText, type Role } from 'react-native';
 
 const textVariants = cva(
   cn(
-    'text-foreground text-base',
+    'text-foreground text-base font-sans',
     Platform.select({
       web: 'select-text',
     })
